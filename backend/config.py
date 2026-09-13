2004c3302e8f8e52aeb0332450a49f516a2e76e9
suryavhi <dassuryavhi704@gmail.com>
2026-09-13 14:11:49 +0530

    Fix Data Location

--- backend/config.py
+++ backend/config.py
@@ -9,7 +9,7 @@ load_dotenv()
 # ------------------------------
 # Paths
 # ------------------------------
-DATA_PATH = r"C:\Users\DELL\Desktop\RAG_Project\data\college.txt"
+DATA_PATH = r"data\college.txt"
 VECTOR_STORE_PATH = "vector_store"
 COLLECTION_NAME = "college_rag_collection"
 
